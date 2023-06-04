e43d71d1097767cd31914487de11d3fb3609513d
sonkarsagar <110252895+sonkarsagar@users.noreply.github.com>
2023-06-04 19:06:30 +0530

    Add files via upload

---
+++ Full stack Expense App/FRONTEND/logIn/login.js
@@ -0,0 +1,17 @@
+
+const email=document.getElementById('email')
+const password=document.getElementById('password')
+const submit=document.getElementById('submit')
+submit.addEventListener('click',(e)=>{
+    e.preventDefault()
+    axios.post('http://127.0.0.1:3000/login',{
+        email: email.value,
+        password: password.value
+    }).then((result) => {
+        alert('LOGGED IN')
+        location.replace("http://127.0.0.1:5500/loggedIn/index.html");
+    }).catch((err) => {
+        // console.log(err.status);
+        alert("WRONG CREDENTIAL OR USER DOESN'T EXISTS.")
+    });
+})
